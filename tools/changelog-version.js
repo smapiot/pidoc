164c9e7e7fad301620956b2d1e1c90ee8c0389a2
Florian Rappl <florian.rappl@smapiot.com>
2021-11-14 14:19:37 +0100

    Changed to monorepo

---
+++ tools/changelog-version.js
@@ -0,0 +1,43 @@
+const { join } = require('path');
+const { readFileSync, writeFileSync } = require('fs');
+
+const defaultPath = join(__dirname, '..', 'CHANGELOG.md');
+
+function getChangelogVersion(changelogPath = defaultPath) {
+  const CHANGELOG = readFileSync(changelogPath, 'utf8');
+  const matches = /^\#\# (\d+\.\d+\.\d+) .*/gm.exec(CHANGELOG);
+
+  if (!matches) {
+    throw new Error('Invalid CHANGELOG format found. Need to fine line starting with "## x.y.z" to get the latest version.');
+  }
+
+  const version = matches[1];
+  return version;
+}
+
+function updateChangelogDate(changelogPath = defaultPath) {
+  const CHANGELOG = readFileSync(changelogPath, 'utf8');
+  const matches = /^\#\# (\d+\.\d+\.\d+) .*/gm.exec(CHANGELOG);
+  const date = new Date().toLocaleDateString('en-us', {
+    month: 'long',
+    year: 'numeric',
+    day: 'numeric',
+  });
+  const newContent = CHANGELOG.replace(matches[0], `## ${matches[1]} (${date})`);
+  writeFileSync(changelogPath, newContent, 'utf8');
+}
+
+if (require.main === module) {
+  const version = getChangelogVersion();
+  const arg = process.argv.pop();
+  console.log(version);
+
+  if (arg === '--update') {
+    updateChangelogDate();
+  }
+} else {
+  module.exports = {
+    getChangelogVersion,
+    updateChangelogDate,
+  };
+}
